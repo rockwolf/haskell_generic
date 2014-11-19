cb9d5d9a2ebf54ff8f748ac186cd66aad491e1c3
rockwolf <thereisanewway@gmail.com>
2014-11-19 14:28:38 +0100

    Better naming and added to comment.

--- DataConversion.hs
+++ DataConversion.hs
@@ -16,20 +16,20 @@ convertListStringToDouble :: [String] -> [Double]
 convertListStringToDouble = map convertToDouble
 
 -- ||| List manipulations
--- | Remove first element from list
+-- | Remove first element from list with groups [a1, b1, c1, a2, b2, c2, ...]
 -- | Example: [20141112;82.3;a comment;20141113;82.1;another comment]
 -- | gives: [82.3;82.1]
-removeFirstFromList [] = []
-removeFirstFromList [x] = []
-removeFirstFromList (x:y:[]) = [y]
-removeFirstFromList (x:y:z:[]) = [y] ++ [z]
-removeFirstFromList (x:y:z:xs) = [y] ++ [z] ++ (removeFirstFromList xs)
+removeFirstFromGroupedList [] = []
+removeFirstFromGroupedList [x] = []
+removeFirstFromGroupedList (x:y:[]) = [y]
+removeFirstFromGroupedList (x:y:z:[]) = [y] ++ [z]
+removeFirstFromGroupedList (x:y:z:xs) = [y] ++ [z] ++ (removeFirstFromList xs)
 
--- | Remove last element from list
+-- | Remove last element from list with groups [a1, b1, c1, a2, b2, c2, ...]
 -- | Example: [20141112;82.3;a comment;20141113;82.1;another comment]
 -- | gives: [20141112;82.3;20141113;82.1]
-removeLastFromList [] = []
-removeLastFromList [x] = []
-removeLastFromList (x:y:[]) = [x] ++ [y]
-removeLastFromList (x:y:z:[]) = [x] ++ [y] ++ [z]
-removeLastFromList (x:y:z:xs) = [x] ++ [y] ++ [z] ++ (removeFirstFromList xs)
+removeLastFromGroupedList [] = []
+removeLastFromGroupedList [x] = []
+removeLastFromGroupedList (x:y:[]) = [x] ++ [y]
+removeLastFromGroupedList (x:y:z:[]) = [x] ++ [y] ++ [z]
+removeLastFromGroupedList (x:y:z:xs) = [x] ++ [y] ++ [z] ++ (removeFirstFromList xs)
