af6166b66938cd5f77685575f5a3cce3910b8c49
rockwolf <thereisanewway@gmail.com>
2014-11-19 15:20:58 +0100

    More functions added.

--- DataConversion.hs
+++ DataConversion.hs
@@ -4,7 +4,29 @@
 
 module DataConversion where
 
+--||| Imports
+import Data.Default.Class
+import Control.Lens hiding (argument)
+import Data.List.Split
+import Data.List
+import Control.Monad (when)
+import System.Exit (exitSuccess)
+
 -- ||| Data conversion methods
+-- | Parses a list of ;-separated string to a list of strings
+-- | Example: ["12;10", "15;5"]
+-- | gives ["12", "10", "15", "5"]
+parseLinesToStringList :: [String] -> [String]
+parseLinesToStringList [] = []
+parseLinesToStringList [x] = parseCurrent x
+parseLinesToStringList (x:xs) = (parseLinesToStringList $ parseCurrent x) ++ parseLinesToStringList xs
+
+-- | Splits a ;-separated string into a list
+-- | Example: "12;10"
+-- | gives ["12", "10"]
+parseCurrent :: String -> [String]
+parseCurrent c = splitOn ";" $ filter (/=' ') c
+
 -- | Convert String to Double datatype
 -- TODO: use reads?
 convertToDouble :: String -> Double
@@ -33,3 +55,20 @@ removeLastFromGroupedList [x] = []
 removeLastFromGroupedList (x:y:[]) = [x] ++ [y]
 removeLastFromGroupedList (x:y:z:[]) = [x] ++ [y] ++ [z]
 removeLastFromGroupedList (x:y:z:xs) = [x] ++ [y] ++ [z] ++ (removeFirstFromList xs)
+
+-- | Turn list into list of lists (2 pairs)
+-- | Example: ["12", "10", "15", 5"]
+-- | gives [["12", "10"], ["15", 5"]]
+convertListToListOfLists :: [a] -> [[a]]
+convertListToListOfLists [] = []
+convertListToListOfLists [x] = []
+convertListToListOfLists (x:y:[]) = [[x] ++ [y]]
+convertListToListOfLists (x:y:xs) = [[x] ++ [y]] ++ (convertListToListOfLists xs)
+
+-- | Drop the last n elements from a list
+dropLastN :: Int -> [a] -> [a]
+dropLastN n xs = reverse $ foldl' (const . drop 1) (reverse xs) (drop n xs)
+
+-- | Get the last n elements from a list
+getLastN :: Int -> [a] -> [a]
+getLastN n xs = foldl' (const . drop 1) xs (drop n xs)
