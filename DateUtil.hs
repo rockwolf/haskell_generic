ff0acf3cd7d150faac3de10e96e1fb99e115ad43
rockwolf <thereisanewway@gmail.com>
2014-11-20 14:38:55 +0100

    Created DateUtil empty structure.

---
+++ DateUtil.hs
@@ -0,0 +1,8 @@
+------------------------------------------------------------------------------
+-- | This module does date/time operations.
+-- See LICENSE file for copyright and license info.
+------------------------------------------------------------------------------
+module DateUtil where
+
+--||| Imports
+-- TODO: what imports are needed?
