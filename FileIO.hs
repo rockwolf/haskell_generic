a5d1803869bc23feae043724c40e9c8953ad1cb5
rockwolf <thereisanewway@gmail.com>
2014-11-20 09:41:55 +0100

    FileIO

---
+++ FileIO.hs
@@ -0,0 +1,7 @@
+------------------------------------------------------------------------------
+-- | This module does file operations and general data loading/saving.
+-- See LICENSE file for copyright and license info.
+------------------------------------------------------------------------------
+module FileIO where
+
+--||| Imports
