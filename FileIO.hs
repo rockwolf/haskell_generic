0223438f36bf4b248a69416d0349129ae33cf927
rockwolf <thereisanewway@gmail.com>
2014-11-20 09:43:32 +0100

    parseFileToStringList

--- FileIO.hs
+++ FileIO.hs
@@ -5,3 +5,10 @@
 module FileIO where
 
 --||| Imports
+-- TODO: what imports are needed?
+
+-- | Return file content as a list of (IO) strings.
+parseFileToStringList :: FilePath -> IO [String]
+parseFileToStringList filename = do
+  my_data <- readFile filename
+  return $ lines my_data
