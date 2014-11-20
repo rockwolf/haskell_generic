f231954a81bdb25aa06c4b9200fa9f690011ad4f
rockwolf <thereisanewway@gmail.com>
2014-11-20 14:41:00 +0100

    Date functions from the
    haskell-chart example.

--- DateUtil.hs
+++ DateUtil.hs
@@ -5,4 +5,16 @@
 module DateUtil where
 
 --||| Imports
--- TODO: what imports are needed?
+import Date.Time.Calendar
+import Date.Time.LocalTime
+
+
+mkDate dd mm yyyy =
+    LocalTime (fromGregorian (fromIntegral yyyy) mm dd) midnight
+    
+mkDateTime dd mm yyyy hh nn =
+    LocalTime (fromGregorian (fromIntegral yyyy) mm dd)
+              (dayFractionToTimeOfDay ((hh*60+nn)/1440))
+              
+mkSeconds ss = LocalTime (fromGregorian (fromIntegral 2009) 11 23)
+                         (dayFractionToTimeOfDay (((14*60+32)*60+ss)/(1440*60)))
